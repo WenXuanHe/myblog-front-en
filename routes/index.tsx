caaf113d1d5c1cc70a199a9acf13655572ee31e5
hewenxuan <hewenxuan@kf5.com>
2017-09-29 10:06:26 +0800

    fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ import * as Router from 'koa-router'
 const router = Router()
 
 router.get('/', async function (ctx, next) {
+  
   if(ctx.session.sessionInfo){
     ctx.redirect('/writer');
   }else{
